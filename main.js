9f396fb7944ebab17b2047dd611f77208527152c
Rebecca Jeon <she4115@gmail.com>
2024-04-22 17:07:03 -0700

    feat: theme added as well as picking up the image source path with os and finding the size and name of the image

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 const path = require('path');
-const {app, BrowserWindow} = require('electron');
+const {app, BrowserWindow, Menu} = require('electron');
+const { create } = require('domain');
 
 const isMac = process.platform === 'darwin';
 const isDev = process.env.NODE_ENV !== 'production'
 
+//Create the main window
 function createMainWindow(){
     const mainWindow = new BrowserWindow({
         title: "Image Resizer",
         width: isDev ? 1000 : 500,
-        height: 600
+        height: 600,
+        webPreferences: {
+          contextIsolation: true,
+          nodeIntegration: true,
+          preload: path.join(__dirname, 'preload.js'),
+        },
     });
 
     //Open devtools if in dev env
@@ -19,15 +26,63 @@ function createMainWindow(){
 
 }
 
+//Create about window
+function createAboutWindow(){
+  const aboutWindow = new BrowserWindow({
+    title: "About Image Resizer",
+    width: 300,
+    height: 300,
+});
+
+aboutWindow.loadFile(path.join(__dirname, './renderer/about.html'));
+}
+//App is ready
 app.whenReady().then(() =>{
     createMainWindow();
    
+    // Implement menu
+    const mainMenu = Menu.buildFromTemplate(menu);
+    Menu.setApplicationMenu(mainMenu);
+
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) {
           createMainWindow()
         }
       })
 })
+
+//Menu template
+const menu = [
+  ...(isMac 
+    ? [
+        {
+          label: app.name,
+          submenu: [
+            {
+              label: 'About',
+              click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : []),
+  {
+    role: 'fileMenu',
+  },
+  ...(!isMac 
+    ? [
+        {
+          label: 'Help',
+          submenu : [
+            {
+            label: 'About',
+            click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : []),
+]
 
 app.on('window-all-closed', () => {
     if (!isMac) {
